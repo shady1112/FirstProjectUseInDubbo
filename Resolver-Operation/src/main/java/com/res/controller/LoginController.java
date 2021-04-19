0ffd6821e828e1d74680af8744469ac1aa5d35e6
Shady <390584929@qq.com>
2021-04-19 22:13:20 +0800

    2

--- Resolver-Operation/src/main/java/com/res/controller/LoginController.java
+++ Resolver-Operation/src/main/java/com/res/controller/LoginController.java
@@ -4,6 +4,8 @@ package com.res.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.res.api.ResolverLoginApi;
 import com.res.entity.request.UserInfoDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +16,11 @@ public class LoginController {
     @Reference
     ResolverLoginApi resolverLoginApi;
 
+    @Autowired
+    RedisTemplate redisTemplate;
 
     @RequestMapping("userlogin")
     public void  userlogin (UserInfoDTO userInfoDTO){
-
         resolverLoginApi.userLogin(userInfoDTO);
     }
 
